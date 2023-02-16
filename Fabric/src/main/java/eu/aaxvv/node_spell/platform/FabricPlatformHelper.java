19e6dbc8b3ef2538c846a9d066fd22378de44209
aaxvv <170654050+aaxvv@users.noreply.github.com>
2023-02-16 21:18:30 +0100

    Initial commit

---
+++ Fabric/src/main/java/eu/aaxvv/node_spell/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package eu.aaxvv.node_spell.platform;
+
+import eu.aaxvv.node_spell.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
